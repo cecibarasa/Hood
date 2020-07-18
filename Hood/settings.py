d1fb4083917cdd619f98d10aff2e23c692070cb3
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-18 12:04:01 +0300

    load boostrap3

--- Hood/settings.py
+++ Hood/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hood.apps.HoodConfig',
+    'hood',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
