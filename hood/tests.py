80d0c23b42775580151577290cb9af99a6bd49d2
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-18 12:38:22 +0300

    add resident test

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class ResidentTestClass(TestCase):
+    def setUp(self):
+        self.cecilia = Resident(first_name = 'Cecilia', last_name= 'Barasa', email = 'ceciheroku@gmail.com')
+
