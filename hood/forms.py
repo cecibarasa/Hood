df58797ede3c7c74d48c247e47e84d92b7d7d949
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-18 15:05:36 +0300

    add signup form

--- hood/forms.py
+++ hood/forms.py
@@ -8,6 +8,10 @@ class HoodForm(forms.ModelForm):
         model = Hood
         fields = ['name', 'location_name','image',]
 
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username","email","password1", "password2"]
 
 class ProfileForm(forms.ModelForm):
     class Meta:
