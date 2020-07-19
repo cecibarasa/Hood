f95cf9b1f9501f2cdaa07ae9d15cb51ab78f149a
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-19 15:17:06 +0300

    run hood test case

--- hood/tests.py
+++ hood/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import *
+from django.contrib.auth.models import User
 
 class ResidentTestClass(TestCase):
     def setUp(self):
@@ -13,3 +14,18 @@ class ResidentTestClass(TestCase):
         residents = Resident.objects.all()
         self.assertTrue(len(residents) > 0)       
 
+class HoodTestClass(TestCase):
+    def setUp(self):
+        self.the_hood =Hood(name='kasa',location_name='kasarani',residents=5)
+        self.the_hood.save()
+
+    def tearDown(self):
+        Hood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.the_hood,Hood))
+
+    def test_save_hood(self):
+        self.the_hood.save_hood()
+        hoods = Hood.objects.all()
+        self.assertTrue(len(hoods) > 0)
